fe2d0d08a6413dd15af0f6f3c3ea0fe5bee64454
Josh Powlison <joshuapowlison@gmail.com>
2018-08-13 19:42:06 -0400

    Make more functionality internal to Looper

--- looper/script.js
+++ looper/script.js
@@ -1,17 +1,19 @@
-function Looper(){
+function Looper(input={}){
+	
 	//It looks like audio buffer's meant for clips <45 seconds long; if that's the case, we might have to use MediaElementAudioSourceNode; but if that's the case, would it just be better to use a bunch of media elements? (if people have song tracks side-by-side greater than 45 seconds- which is extremely likely- we'll need to use media elements and line them up, in which case this whole setup seems unnecessary)
 	
 	//divs filled with audio elements, like characters? Connect the correct ones, save a common time? (the trick will be, I think, lining them up. One of the elements would have to be the reference against which every other piece is checked; probably layer 0)
 	
 	const L=this;
-	
 	L.context=new AudioContext();
 	L.loops=0;
-	var loopInterval=null;
+	L.track=input.track;
+	L.path=input.path;
 	
-	//Store all audio clips in this here buffer
+	var loopInterval=null;
 	var buffer={};
 	var sources={};
+	var loopDuration=0;
 	
 	//Load files from an array
 	L.load=function(files){
@@ -19,7 +21,7 @@ function Looper(){
 		
 		//BUFFER FILES//
 		for(var i=0;i<files.length;i++){
-			let fileName=files[i];
+			let fileName=L.path+'/'+files[i];
 		
 			//Skip over buffered or buffering tracks
 			if(buffer[fileName]) continue;
@@ -41,22 +43,24 @@ function Looper(){
 		}
 		
 		//Return a promise with the info
-		return Promise.all(promises);
+		Promise.all(promises).then((values)=>{
+			loopDuration=values[0].duration;
+		});
 	}
 	
 	//Play the track
-	L.playTrack=function(track){
-		loopInterval=setInterval(function(){playLoop(track);},Math.floor(loopDuration*1000));
+	L.playTrack=function(){
+		loopInterval=setInterval(playLoop,Math.floor(loopDuration*1000));
 		L.loops=0;
 		
-		playLoop(track);
+		playLoop();
 	}
 	
-	function playLoop(track){
-		for(var i=0;i<music[track].length;i++){
-			var get=L.loops % music[track][i].length;
+	function playLoop(){
+		for(var i=0;i<L.track.length;i++){
+			var get=L.loops % L.track[i].length;
 			
-			var fileName=track+'/'+music[track][i][get];
+			var fileName=L.path+'/'+L.track[i][get];
 			
 			var source=L.context.createBufferSource();
 			source.buffer=buffer[fileName];
@@ -64,25 +68,26 @@ function Looper(){
 			source.start(0);
 			
 			//Track sources
-			if(!sources[track]) sources[track]={};
-			sources[track][fileName]=source;
+			if(!sources[L.track]) sources[L.track]={};
+			sources[L.track][fileName]=source;
 		}
 		
 		L.loops++;
 	}
 	
-	L.stopTrack=function(track){
-		for(var file in sources[track]){
-			sources[track][file].stop(0);
+	L.stopTrack=function(){
+		for(var file in sources[L.track]){
+			sources[L.track][file].stop(0);
 		}
 		
 		clearInterval(loopInterval);
+		loopInterval=null;
 	}
 	
-	L.adjustLayer=function(track,layer,input){
+	L.adjustLayer=function(layer,input){
 		//for(
 		//sources[track][
 	
-		music[track][layer]=input;
+		L.track[layer]=input;
 	}
 }
